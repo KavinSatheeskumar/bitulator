7ead7951050daaeb532a6985ec0dd5e8f5644604
Pavlos <79235112+pavlosconstas@users.noreply.github.com>
2021-09-18 10:06:49 -0400

    Spawn ./test-script
    Co-Authored-By: Kavin Satheeskumar <71559920+KavinSatheeskumar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
+const { execFile } = require('child_process');
 const app = express();
 const port = 80;
 
 app.use(express.static(path.join(__dirname, './frontend/build')));
 
-app.get('/', (req, res) => {
-    
+
+app.get('/api', (req, res) => {
+    const child = execFile(path.join(__dirname, './compiler/test-script'), ['Pavlos'])
+    child.stdout.on('data', data => {
+        res.send(data)
+    })
 })  
 
 app.listen(port, () => {
