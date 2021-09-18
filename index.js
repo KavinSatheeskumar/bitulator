40972cc11d9986d897ee1a4b0fc3ec78a3663b4f
Pavlos <79235112+pavlosconstas@users.noreply.github.com>
2021-09-18 09:03:43 -0400

    Serve react app using express

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 80;
 
+app.use(express.static(path.join(__dirname, './frontend/bitulator/build')));
+
 app.get('/', (req, res) => {
-    res.sendFile('./frontend/bitulator/public/index.html')
+    
 })  
 
 app.listen(port, () => {
