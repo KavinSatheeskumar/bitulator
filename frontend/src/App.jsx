b970790d24e3af46f8c2620468b53e41de6e3796
KavinSatheeskumar <kavinsathees@gmail.com>
2021-09-18 15:11:48 -0400

    Adding basic animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import * as d3 from 'd3';
 import axios from "axios";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [response, setResponse] = useState();
+  const [instrs, setInstrs] = useState(["a","b","c","d"]);
+  const [isAnimating, setAnimating] = useState(false);
+  const [currentInstr, setCurrentInstr] = useState(-1);
+  const instructionsRef = useRef()
+
+  useEffect(() => {
+    if (isAnimating) {
+      d3.select("#Code")
+        .transition()
+        .delay(1000)
+        .style("background-color", "red");
+    }
+    setAnimating(false);
+  })
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,6 +32,11 @@ const App = () => {
       })
       .catch((error) => console.error(error));
   };
+
+  const Animate = (e) => {
+    setAnimating(true);
+  }
+
   return (
     <div className="App">
       <div className="input">
@@ -35,6 +55,22 @@ const App = () => {
         </form>
       </div>
       <p>{response}</p>
+      <button onClick={Animate}>Click to do fancy stuff</button>
+      <div className="Instructions">
+        <table 
+          id="Code"
+          ref={instructionsRef}
+        >
+          <tbody>
+            {instrs.map((str, i) => (
+                <tr>
+                  <td>{str}</td>
+                  <td>{i === currentInstr ? "===" : "---"}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
