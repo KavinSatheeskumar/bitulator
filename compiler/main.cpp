1cc36663de859a9a91c68cfc2bf8938c33e809a4
Pavlos <79235112+pavlosconstas@users.noreply.github.com>
2021-09-18 17:23:38 -0400

    Update main.cpp

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -9,5 +9,4 @@ int main(int argc, char* argv[]) {
     //remove useless whitespace
     input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
     std::vector<Tokens> tmp = getchars(input);
-    parse(tmp);
 }; 
