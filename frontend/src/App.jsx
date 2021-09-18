92d08f5f639ccb6932322e766dd17ae55ddb71da
KavinSatheeskumar <kavinsathees@gmail.com>
2021-09-18 10:16:06 -0400

    Converting App into a class component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  render () {
+    return (
+        <div className="App">
+          <p>This is my new text</p>
+        </div>
+      )
+  }
+}
+
+export default App;
