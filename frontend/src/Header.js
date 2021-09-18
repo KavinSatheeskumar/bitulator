564656a94ac79bf920bc2680edc019c0a04098f4
Pavlos <79235112+pavlosconstas@users.noreply.github.com>
2021-09-18 15:42:41 -0400

    css and header

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    return(
+        <div className="header">
+            <h2>Bitulator</h2>
+            <div className="nav-links">
+                <a href="https://github.com/KavinSatheeskumar/bitulator">Github</a>
+            </div>
+        </div>
+    )
+
+};
+
+export default Header;
