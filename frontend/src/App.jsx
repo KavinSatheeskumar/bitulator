5d1fb2c17c5497edfe74c9839462ca6579ff55d6
KavinSatheeskumar <kavinsathees@gmail.com>
2021-09-18 22:27:37 -0400

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,8 +68,8 @@ const App = () => {
     let SP = 0;
 
     for (let i = 0; i < instrs.length; ++i) {
-      let arr = []
-      if (allMemStates == []){
+      let arr = [];
+      if (allMemStates === []){
         for (let j = 0; j < MEM_HIG; ++j){
           let sub_arr = []
           for(let k = 0; k < MEM_WID; ++k){
@@ -78,8 +78,15 @@ const App = () => {
           arr.push(sub_arr);
         }
       } else {
-        arr = JSON.parse(JSON.stringify(allMemStates[allMemStates.length - 1]));
+        for (let j = 0; j < MEM_HIG; ++j){
+          let sub_arr = []
+            for(let k = 0; k < MEM_WID; ++k){
+              sub_arr.push(allMemStates[j][k]);
+            }
+          arr.push(sub_arr);
+        }
       }
+      allMemStates.push(arr);
 
       let op = instrs[i][0];
       let loc = instrs[i][1];
